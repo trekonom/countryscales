7e9a1022bba075463c028aad753161f0d3eff2b7
stefan <moogs@gmx.de>
2022-01-09 02:25:47 +0100

    * Add general label_euro and euro functions. * Hard-code big.mark and decimal.mark in "_de" label functions.

--- R/label-euro-de.R
+++ R/label-euro-de.R
@@ -15,40 +15,75 @@
 #' @export
 #'
 #' @example inst/ex/ex-label_euro_de.R
-label_euro_de <- function(accuracy = 1, scale = 1, big.mark = ".", decimal.mark = ",", prefix = "", suffix = "\u20ac", trim = TRUE, ...) {
+label_euro <- function(accuracy = NULL, scale = 1, big.mark = ",", decimal.mark = ".", prefix = "", suffix = .euro, trim = TRUE,
+                       largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
   scales::label_dollar(
     accuracy = accuracy, scale = scale,
     big.mark = big.mark, decimal.mark = decimal.mark,
-    prefix = prefix, suffix = suffix, trim = trim, ...
+    prefix = prefix, suffix = suffix, trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens,
+    ...
   )
 }
 
-#' @rdname label_euro_de
+#' @rdname label_euro
 #' @export
-euro_de <- function(x, accuracy = 1, scale = 1, big.mark = ".", decimal.mark = ",", prefix = "", suffix = "\u20ac", trim = TRUE, ...) {
+euro <- function(x, accuracy = 1, scale = 1, big.mark = ".", decimal.mark = ",", prefix = "", suffix = .euro, trim = TRUE,
+                 largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
   scales::label_dollar(
     accuracy = accuracy, scale = scale,
     big.mark = big.mark, decimal.mark = decimal.mark,
-    prefix = prefix, suffix = suffix, trim = trim, ...
+    prefix = prefix, suffix = suffix, trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens, ...
   )(x)
 }
 
-#' @rdname label_euro_de
+#' @rdname label_euro
 #' @export
-label_dollar_de <- function(accuracy = 1, scale = 1, big.mark = ".", decimal.mark = ",", prefix = "$", suffix = "", trim = TRUE, ...) {
+label_euro_de <- function(accuracy = 1, scale = 1, suffix = .euro, trim = TRUE,
+                          largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
   scales::label_dollar(
     accuracy = accuracy, scale = scale,
-    big.mark = big.mark, decimal.mark = decimal.mark,
-    prefix = prefix, suffix = suffix, trim = trim, ...
+    big.mark = ".", decimal.mark = ",",
+    prefix = "", suffix = suffix, trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens,
+    ...
   )
 }
 
-#' @rdname label_euro_de
+#' @rdname label_euro
 #' @export
-dollar_de <- function(x, accuracy = 1, scale = 1, big.mark = ".", decimal.mark = ",", prefix = "$", suffix = "", trim = TRUE, ...) {
-  scales::label_dollar(x,
+euro_de <- function(x, accuracy = 1, scale = 1, suffix = .euro, trim = TRUE,
+                    largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
+  scales::label_dollar(
     accuracy = accuracy, scale = scale,
-    big.mark = big.mark, decimal.mark = decimal.mark,
-    prefix = prefix, suffix = suffix, trim = trim, ...
+    big.mark = ".", decimal.mark = ",",
+    prefix = "", suffix = suffix, trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens,
+    ...
+  )(x)
+}
+
+#' @rdname label_euro
+#' @export
+label_dollar_de <- function(accuracy = 1, scale = 1, prefix = .dollar, trim = TRUE,
+                            largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
+  scales::label_dollar(
+    accuracy = accuracy, scale = scale,
+    big.mark = ".", decimal.mark = ",",
+    prefix = prefix, suffix = "", trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens, ...
+  )
+}
+
+#' @rdname label_euro
+#' @export
+dollar_de <- function(x, accuracy = 1, scale = 1, prefix = .dollar, trim = TRUE,
+                      largest_with_cents = 1e+05, negative_parens = FALSE, ...) {
+  scales::label_dollar(
+    accuracy = accuracy, scale = scale,
+    big.mark = ".", decimal.mark = ",",
+    prefix = prefix, suffix = "", trim = trim,
+    largest_with_cents = largest_with_cents, negative_parens = negative_parens, ...
   )(x)
 }
