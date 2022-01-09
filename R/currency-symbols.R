314da60f6082248e39bd5644141a74d985d40cda
stefan <moogs@gmx.de>
2022-01-09 02:21:26 +0100

    Add currency symbols.

---
+++ R/currency-symbols.R
@@ -0,0 +1,19 @@
+#' Currency symbols
+#'
+#' @name currency-symbols
+#' @keywords internal
+#' @aliases NULL
+NULL
+
+#' @export
+#' @rdname currency-symbols
+.euro <- "\u20AC"
+#' @export
+#' @rdname currency-symbols
+.dollar <- "\u0024"
+#' @export
+#' @rdname currency-symbols
+.pound <- "\u00A3"
+#' @export
+#' @rdname currency-symbols
+.yen <- .renminbi <- .yuan <- "\u00a5"
--- inst/ex/ex-label_euro_de.R
+++ inst/ex/ex-label_euro_de.R
@@ -7,9 +7,9 @@ dollar_de(x, accuracy = .1)
 # For other currencies or if you want to switch the position
 # of the currency symbol switch the suffix and/or prefix
 # Dollar symbol after number
-euro_de(x, suffix = "$")
+euro_de(x, suffix = .dollar)
 # British pound before number
-dollar_de(x, prefix = "\u00a3")
+dollar_de(x, prefix = .pound)
 # For use as labeller e.g. with ggplot2
 library(ggplot2)
 p <- ggplot(gapminder15, aes(gdp_per_cap, life_exp)) +
