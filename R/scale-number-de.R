c0a8e4c7269d173262481d14b57f93849d2834c3
stefan <moogs@gmx.de>
2022-01-16 23:15:38 +0100

    Add scale_x/y_number.

--- R/scale-number-de.R
+++ R/scale-number-de.R
@@ -12,10 +12,120 @@
 #' * the `_euro` ones format axis text as euros.
 #'
 #' @inheritParams ggplot2::scale_x_continuous
-#' @inheritParams label_number_de
+#' @inheritParams scales::label_number
 #' @param prefix Symbol to display before value.
 #' @param suffix Symbol to display after value.
 #'
+#' @export
+scale_x_number <- function(name = waiver(),
+                           breaks = waiver(),
+                           minor_breaks = waiver(),
+                           guide = waiver(),
+                           n.breaks = NULL,
+                           labels,
+                           limits = NULL,
+                           expand = c(0.01, 0),
+                           oob = censor,
+                           na.value = NA_real_,
+                           trans = "identity",
+                           position = "bottom",
+                           sec.axis = waiver(),
+                           accuracy = 1, scale = 1,
+                           prefix = "", suffix = "",
+                           big.mark = " ", decimal.mark = ".",
+                           trim = TRUE, ...) {
+  labels <- if (missing(labels)) {
+    label_number(
+      accuracy = accuracy,
+      scale = scale,
+      prefix = prefix,
+      suffix = suffix,
+      big.mark = big.mark,
+      decimal.mark = decimal.mark,
+      trim = trim,
+      ...
+    )
+  }
+
+  sc <- continuous_scale(
+    aesthetics = gscales_global$x_aes,
+    scale_name = "position_c",
+    palette = identity,
+    name = name,
+    breaks = breaks,
+    n.breaks = n.breaks,
+    minor_breaks = minor_breaks,
+    labels = labels,
+    limits = limits,
+    expand = expand,
+    oob = oob,
+    na.value = na.value,
+    trans = trans,
+    guide = guide,
+    position = position,
+    super = ScaleContinuousPosition
+  )
+
+  set_sec_axis(sec.axis, sc)
+}
+
+#' @rdname scale_x_number
+#' @export
+scale_y_number <- function(name = waiver(),
+                           breaks = waiver(),
+                           minor_breaks = waiver(),
+                           guide = waiver(),
+                           n.breaks = NULL,
+                           labels,
+                           limits = NULL,
+                           expand = c(0.01, 0),
+                           oob = censor,
+                           na.value = NA_real_,
+                           trans = "identity",
+                           position = "left",
+                           sec.axis = waiver(),
+                           accuracy = 1, scale = 1,
+                           prefix = "", suffix = "",
+                           big.mark = " ", decimal.mark = ".",
+                           trim = TRUE, ...) {
+  labels <- if (missing(labels)) {
+    label_number(
+      accuracy = accuracy,
+      scale = scale,
+      prefix = prefix,
+      suffix = suffix,
+      big.mark = big.mark,
+      decimal.mark = decimal.mark,
+      trim = trim,
+      ...
+    )
+  }
+
+  sc <- continuous_scale(
+    aesthetics = gscales_global$y_aes,
+    scale_name = "position_c",
+    palette = identity,
+    name = name,
+    breaks = breaks,
+    n.breaks = n.breaks,
+    minor_breaks = minor_breaks,
+    labels = labels,
+    limits = limits,
+    expand = expand,
+    oob = oob,
+    na.value = na.value,
+    trans = trans,
+    guide = guide,
+    position = position,
+    super = ScaleContinuousPosition
+  )
+
+  sc <- set_sec_axis(sec.axis, sc)
+
+  sc
+}
+
+#' @rdname scale_x_number
 #' @export
 scale_x_number_de <- function(name = waiver(),
                               breaks = waiver(),
@@ -69,7 +179,7 @@ scale_x_number_de <- function(name = waiver(),
   set_sec_axis(sec.axis, sc)
 }
 
-#' @rdname scale_x_number_de
+#' @rdname scale_x_number
 #' @export
 scale_y_number_de <- function(name = waiver(),
                               breaks = waiver(),
