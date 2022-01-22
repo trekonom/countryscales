804778ae88bf2fcbe35a64df38b4947d780ce125
stefan <moogs@gmx.de>
2022-01-22 09:48:38 +0100

    Rename package.

--- R/countryscales-global.R
+++ R/countryscales-global.R
@@ -1,15 +1,15 @@
 # Adapted from gggplot2
 #
 # Environment that holds various global settings for ggplot.
-gscales_global <- new.env(parent = emptyenv())
+countryscales_global <- new.env(parent = emptyenv())
 
 # x aesthetics
-gscales_global$x_aes <- c(
+countryscales_global$x_aes <- c(
   "x", "xmin", "xmax", "xend", "xintercept",
   "xmin_final", "xmax_final", "xlower", "xmiddle", "xupper", "x0"
 )
 
-gscales_global$y_aes <- c(
+countryscales_global$y_aes <- c(
   "y", "ymin", "ymax", "yend", "yintercept",
   "ymin_final", "ymax_final", "lower", "middle", "upper", "y0"
 )
