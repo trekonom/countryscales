f77b8f277876821cce42a96eac96dac1d88593ee
stefan <moogs@gmx.de>
2022-09-08 00:07:18 +0200

    Add imports

--- R/countryscales-package.R
+++ R/countryscales-package.R
@@ -1,8 +1,9 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @importFrom ggplot2 waiver sec_axis ScaleContinuousPosition continuous_scale
-#' @importFrom scales censor
+#' @importFrom ggplot2 waiver sec_axis ScaleContinuousPosition ScaleContinuous continuous_scale
+#' @importFrom scales censor demo_continuous number_format
+#' @importFrom utils modifyList
 
 ## usethis namespace: start
 ## usethis namespace: end
