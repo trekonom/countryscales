ecf473dd8d57551e4445318bef98bbf4956ca4d0
stefan <moogs@gmx.de>
2022-11-04 00:46:01 +0100

    Add locale argument.

--- R/scale-number.R
+++ R/scale-number.R
@@ -12,6 +12,7 @@
 #' @inheritParams scales::label_number
 #' @param prefix Symbol to display before value.
 #' @param suffix Symbol to display after value.
+#' @param locale locale string. Defaults to "en_US"
 #'
 #' @name scale-format
 NULL
@@ -19,22 +20,31 @@ NULL
 #' @rdname scale-format
 #' @export
 scale_x_number <- function(name = waiver(),
-                           breaks = waiver(),
-                           minor_breaks = waiver(),
-                           guide = waiver(),
-                           n.breaks = NULL,
-                           labels = waiver(),
-                           limits = NULL,
-                           expand = waiver(),
-                           oob = censor,
-                           na.value = NA_real_,
-                           trans = "identity",
-                           position = "bottom",
-                           sec.axis = waiver(),
-                           accuracy = 1, scale = 1,
-                           prefix = "", suffix = "",
-                           big.mark = " ", decimal.mark = ".",
-                           trim = TRUE, ...) {
+                                  breaks = waiver(),
+                                  minor_breaks = waiver(),
+                                  guide = waiver(),
+                                  n.breaks = NULL,
+                                  labels = waiver(),
+                                  limits = NULL,
+                                  expand = waiver(),
+                                  oob = censor,
+                                  na.value = NA_real_,
+                                  trans = "identity",
+                                  position = "bottom",
+                                  sec.axis = waiver(),
+                                  accuracy = 1, scale = 1,
+                                  prefix = "", suffix = "",
+                                  big.mark = NULL,
+                                  decimal.mark = NULL,
+                                  locale = "en_US",
+                                  trim = TRUE, ...) {
+
+  locale <- match.arg(locale, unique(countryscales::locales$locale))
+  locale <- countryscales::locales[countryscales::locales$locale %in% locale, ]
+
+  big.mark <- big.mark %||% locale[["thousands_sep"]]
+  decimal.mark <- decimal.mark %||% locale[["decimal_point"]]
+
   sc <- number_scale(
     aesthetics = countryscales_global$x_aes,
     scale_name = "position_c",
@@ -73,60 +83,23 @@ scale_y_number <- function(name = waiver(),
                            oob = censor,
                            na.value = NA_real_,
                            trans = "identity",
-                           position = "left",
+                           position = "bottom",
                            sec.axis = waiver(),
                            accuracy = 1, scale = 1,
                            prefix = "", suffix = "",
-                           big.mark = " ", decimal.mark = ".",
+                           big.mark = NULL,
+                           decimal.mark = NULL,
+                           locale = "en_US",
                            trim = TRUE, ...) {
+
+  locale <- match.arg(locale, unique(countryscales::locales$locale))
+  locale <- countryscales::locales[countryscales::locales$locale %in% locale, ]
+
+  big.mark <- big.mark %||% locale[["thousands_sep"]]
+  decimal.mark <- decimal.mark %||% locale[["decimal_point"]]
+
   sc <- number_scale(
     aesthetics = countryscales_global$y_aes,
-    scale_name = "position_c",
-    palette = identity,
-    name = name,
-    breaks = breaks,
-    n.breaks = n.breaks,
-    minor_breaks = minor_breaks,
-    labels = labels,
-    limits = limits,
-    expand = expand,
-    oob = oob,
-    na.value = na.value,
-    trans = trans,
-    guide = guide,
-    position = position,
-    accuracy = accuracy, scale = scale,
-    prefix = prefix, suffix = suffix,
-    big.mark = big.mark, decimal.mark = decimal.mark,
-    super = ScaleContinuousPosition
-  )
-
-  sc <- set_sec_axis(sec.axis, sc)
-
-  sc
-}
-
-#' @rdname scale-format
-#' @export
-scale_x_percent <- function(name = waiver(),
-                            breaks = waiver(),
-                            minor_breaks = waiver(),
-                            guide = waiver(),
-                            n.breaks = NULL,
-                            labels = waiver(),
-                            limits = NULL,
-                            expand = waiver(),
-                            oob = censor,
-                            na.value = NA_real_,
-                            trans = "identity",
-                            position = "bottom",
-                            sec.axis = waiver(),
-                            accuracy = 1, scale = 100,
-                            prefix = "", suffix = "%",
-                            big.mark = " ", decimal.mark = ".",
-                            trim = TRUE, ...) {
-  sc <- number_scale(
-    aesthetics = countryscales_global$x_aes,
     scale_name = "position_c",
     palette = identity,
     name = name,
@@ -152,6 +125,61 @@ scale_x_percent <- function(name = waiver(),
 
 #' @rdname scale-format
 #' @export
+scale_x_percent <- function(name = waiver(),
+                           breaks = waiver(),
+                           minor_breaks = waiver(),
+                           guide = waiver(),
+                           n.breaks = NULL,
+                           labels = waiver(),
+                           limits = NULL,
+                           expand = waiver(),
+                           oob = censor,
+                           na.value = NA_real_,
+                           trans = "identity",
+                           position = "bottom",
+                           sec.axis = waiver(),
+                           accuracy = 1, scale = 100,
+                           prefix = "", suffix = NULL,
+                           big.mark = NULL,
+                           decimal.mark = NULL,
+                           locale = "en_US",
+                           trim = TRUE, ...) {
+
+  locale <- match.arg(locale, unique(countryscales::locales$locale))
+  locale <- countryscales::locales[countryscales::locales$locale %in% locale, ]
+
+  big.mark <- big.mark %||% locale[["thousands_sep"]]
+  decimal.mark <- decimal.mark %||% locale[["decimal_point"]]
+  suffix <- suffix %||% paste0(rep(" ", locale[["p_sep_by_space"]]), "%")
+
+  sc <- number_scale(
+    aesthetics = countryscales_global$x_aes,
+    scale_name = "position_c",
+    palette = identity,
+    name = name,
+    breaks = breaks,
+    n.breaks = n.breaks,
+    minor_breaks = minor_breaks,
+    labels = labels,
+    limits = limits,
+    expand = expand,
+    oob = oob,
+    na.value = na.value,
+    trans = trans,
+    guide = guide,
+    position = position,
+    accuracy = accuracy, scale = scale,
+    prefix = prefix, suffix = suffix,
+    big.mark = big.mark, decimal.mark = decimal.mark,
+    super = ScaleContinuousPosition
+  )
+
+  set_sec_axis(sec.axis, sc)
+}
+
+
+#' @rdname scale-format
+#' @export
 scale_y_percent <- function(name = waiver(),
                             breaks = waiver(),
                             minor_breaks = waiver(),
@@ -163,12 +191,22 @@ scale_y_percent <- function(name = waiver(),
                             oob = censor,
                             na.value = NA_real_,
                             trans = "identity",
-                            position = "left",
+                            position = "bottom",
                             sec.axis = waiver(),
                             accuracy = 1, scale = 100,
-                            prefix = "", suffix = "%",
-                            big.mark = " ", decimal.mark = ".",
+                            prefix = "", suffix = NULL,
+                            big.mark = NULL,
+                            decimal.mark = NULL,
+                            locale = "en_US",
                             trim = TRUE, ...) {
+
+  locale <- match.arg(locale, unique(countryscales::locales$locale))
+  locale <- countryscales::locales[countryscales::locales$locale %in% locale, ]
+
+  big.mark <- big.mark %||% locale[["thousands_sep"]]
+  decimal.mark <- decimal.mark %||% locale[["decimal_point"]]
+  suffix <- suffix %||% paste0(rep(" ", locale[["p_sep_by_space"]]), "%")
+
   sc <- number_scale(
     aesthetics = countryscales_global$y_aes,
     scale_name = "position_c",
@@ -191,10 +229,9 @@ scale_y_percent <- function(name = waiver(),
     super = ScaleContinuousPosition
   )
 
-  sc <- set_sec_axis(sec.axis, sc)
-
-  sc
+  set_sec_axis(sec.axis, sc)
 }
+
 
 #' @rdname scale-format
 #' @export
