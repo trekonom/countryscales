01996f4a1b0a90a732492205a94852ab9b021549
stefan <moogs@gmx.de>
2022-11-04 20:23:02 +0100

    Add show_locales

---
+++ R/show-locales.R
@@ -0,0 +1,15 @@
+#' Locales
+#'
+#' A convenience function which returns a dataframe of available locales and countries.
+#'
+#' @return a dataframe
+#'
+#' @export
+#'
+#' @examples
+#' show_locales()
+show_locales <- function() {
+  x <- countryscales::locales[c("locale", "cntry")]
+  x$locale <- gsub("_", "-", x$locale)
+  return(x)
+}
