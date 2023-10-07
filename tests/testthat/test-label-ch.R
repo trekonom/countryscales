3f45737c30a9f56f2dd2bb378fa52fc5ab7bda6b
trekonom <moogs@gmx.de>
2023-10-07 07:44:42 +0200

    Add tests for label_percent_xx.

--- tests/testthat/test-label-ch.R
+++ tests/testthat/test-label-ch.R
@@ -1,4 +1,6 @@
 test_that("uses a ’ as big.mark and a . as decimal mark", {
   expect_equal(label_number_ch()(1200.243), "1\u2019200")
   expect_equal(label_number_ch(accuracy = .1)(1.243), "1.2")
+  expect_equal(label_percent_ch()(.243), "24 %")
+  expect_equal(label_percent_ch(accuracy = .1)(.243), "24.3 %")
 })
--- tests/testthat/test-label-de.R
+++ tests/testthat/test-label-de.R
@@ -1,4 +1,6 @@
 test_that("uses a . as big.mark and a , as decimal mark", {
   expect_equal(label_number_de()(1200.243), "1.200")
   expect_equal(label_number_de(accuracy = .1)(1.243), "1,2")
+  expect_equal(label_percent_de()(.243), "24 %")
+  expect_equal(label_percent_de(accuracy = .1)(.243), "24,3 %")
 })
