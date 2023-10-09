c79de1db9ea09bfbbf0be0203b079b966ad52946
trekonom <moogs@gmx.de>
2023-10-09 23:02:48 +0200

    Update test.

--- tests/testthat/test-label-locale.R
+++ tests/testthat/test-label-locale.R
@@ -2,33 +2,79 @@ dat <- readRDS(
   system.file("extdata", "testloc.rds", package = "countryscales")
 )
 
-x <- 123456.789
-y <- .789
-z <- x
-
 test_that("label_percent_locale works with prefixed percent symbol", {
-  locale <- "eu"
-  expect_equal(
-    label_percent_locale(locale = locale)(c(y, -y)),
-    unlist(
-      dat[dat$locale == locale, c("percent_pos", "percent_neg")],
-      use.names = FALSE
+  value <- .789
+  locales <- c("eu", "tr", "ku")
+  lapply(locales, \(locale) {
+    expect_equal(
+      label_percent_locale(locale = locale)(c(value, -value)),
+      unlist(
+        dat[dat$locale == locale, c("percent_pos", "percent_neg")],
+        use.names = FALSE
+      )
     )
-  )
-  locale <- "tr"
-  expect_equal(
-    label_percent_locale(locale = locale)(c(y, -y)),
-    unlist(
-      dat[dat$locale == locale, c("percent_pos", "percent_neg")],
-      use.names = FALSE
-    )
-  )
-  locale <- "ku"
-  expect_equal(
-    label_percent_locale(locale = locale)(c(y, -y)),
-    unlist(
-      dat[dat$locale == locale, c("percent_pos", "percent_neg")],
-      use.names = FALSE
-    )
-  )
+  })
 })
+
+test_that("label_number_locale works", {
+  value <- 123456
+  lapply(locales$locale, \(locale) {
+    expect_equal(
+      paste(
+        locale,
+        label_number_locale(locale = locale)(c(value, -value)),
+        sep = ": "
+      ),
+      paste(
+        locale,
+        unlist(
+          dat[dat$locale == locale, c("number_pos", "number_neg")],
+          use.names = FALSE
+        ),
+        sep = ": "
+      )
+    )
+  })
+})
+
+test_that("label_currency_locale works", {
+  value <- 123456
+  lapply(locales$locale, \(locale) {
+    expect_equal(
+      paste(
+        locale,
+        label_currency_locale(locale = locale)(c(value, -value)),
+        sep = ": "
+      ),
+      paste(
+        locale,
+        unlist(
+          dat[dat$locale == locale, c("currency_pos", "currency_neg")],
+          use.names = FALSE
+        ),
+        sep = ": "
+      )
+    )
+  })
+})
+
+# test_that("label_percent_locale works", {
+#   value <- .789
+#   lapply(locales$locale, \(locale) {
+#     expect_equal(
+#       paste(
+#         locale,
+#         label_percent_locale(locale = locale)(c(value, -value)),
+#         sep = ": "
+#       ),
+#       paste(
+#         locale,
+#         unlist(
+#           dat[dat$locale == locale, c("percent_pos", "percent_neg")],
+#           use.names = FALSE
+#         ),
+#         sep = ": "
+#       )
+#     )
+#   })
+# })
