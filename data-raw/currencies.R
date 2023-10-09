4b6356401f17947391cceb9b2128b9069c4bf82c
trekonom <moogs@gmx.de>
2023-10-09 22:59:07 +0200

    Fix. Use Intl currency symbols.

--- data-raw/currencies.R
+++ data-raw/currencies.R
@@ -2,4 +2,15 @@
 
 data(currencies, package = "i18n")
 
+currencies <- currencies |>
+  mutate(
+    # Fixes.
+    currency_symbol = case_when(
+      locale %in% c(
+        "xh", "und",
+        "nds", "nds-NL", "lag"
+      ) & currency_code == "USD" ~ "$",
+      .default = currency_symbol
+    )
+  )
 usethis::use_data(currencies, overwrite = TRUE)
