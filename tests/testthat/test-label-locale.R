49240d73c88ad00f1f94784898dbfaf1807e3dd8
trekonom <moogs@gmx.de>
2023-10-10 10:53:11 +0200

    Update tests. Convert to latin numerals to arab numerals for locales with default arab numbering system.

--- tests/testthat/test-label-locale.R
+++ tests/testthat/test-label-locale.R
@@ -16,13 +16,36 @@ test_that("label_percent_locale works with prefixed percent symbol", {
   })
 })
 
+latn <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "%", ".", ",")
+arab <- c(
+  "\u0661", "\u0662", "\u0663", "\u0664", "\u0665", "\u0666", "\u0667",
+  "\u0668", "\u0669", "\u0660",
+  "\u066a", "\u066b", "\u066c"
+)
+
+latn <- paste(latn, collapse = "")
+arab <- paste(arab, collapse = "")
+
+as_arab <- function(x) {
+  chartr(latn, arab, x)
+}
+
 test_that("label_number_locale works", {
   value <- 123456
   lapply(locales$locale, \(locale) {
+    x <- label_number_locale(locale = locale)(c(value, -value))
+    default_numbering_system <- locales[
+      locales$locale == locale,
+      "default_numbering_system",
+      drop = TRUE
+    ]
+    if (default_numbering_system == "arab") {
+      x <- as_arab(x)
+    }
     expect_equal(
       paste(
         locale,
-        label_number_locale(locale = locale)(c(value, -value)),
+        x,
         sep = ": "
       ),
       paste(
@@ -40,10 +63,20 @@ test_that("label_number_locale works", {
 test_that("label_currency_locale works", {
   value <- 123456
   lapply(locales$locale, \(locale) {
+    x <- label_currency_locale(locale = locale)(c(value, -value))
+    default_numbering_system <- locales[
+      locales$locale == locale,
+      "default_numbering_system",
+      drop = TRUE
+    ]
+    if (default_numbering_system == "arab") {
+      x <- as_arab(x)
+    }
+
     expect_equal(
       paste(
         locale,
-        label_currency_locale(locale = locale)(c(value, -value)),
+        x,
         sep = ": "
       ),
       paste(
@@ -61,10 +94,21 @@ test_that("label_currency_locale works", {
 test_that("label_percent_locale works", {
   value <- .789
   lapply(locales$locale, \(locale) {
+    x <- label_percent_locale(locale = locale)(c(value, -value))
+    default_numbering_system <- locales[
+      locales$locale == locale,
+      "default_numbering_system",
+      drop = TRUE
+    ]
+    if (default_numbering_system == "arab") {
+      #paste0(x, "\u061c")
+      x <- as_arab(x)
+    }
+
     expect_equal(
       paste(
         locale,
-        label_percent_locale(locale = locale)(c(value, -value)),
+        x,
         sep = ": "
       ),
       paste(
